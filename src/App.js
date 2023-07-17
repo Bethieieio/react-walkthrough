fa8e7a230946567773a3655591eb32f32520b042
Beth Allaway <beth_allaway@yahoo.com>
2023-07-17 21:43:48 +0100

    add nav bar stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Button from 'react-bootstrap/Button';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
+      <NavBar/>
     </div>
   );
 }
 
-export default App;
+export default App
