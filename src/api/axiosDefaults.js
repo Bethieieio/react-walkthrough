b99e2f36256f336a6e54ae2bc7087d58d01fd169
Beth Allaway <beth_allaway@yahoo.com>
2023-07-26 21:48:51 +0100

    make sign up page work

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://react-backend-api-1cc3d70f4b60.herokuapp.com/';
+// axios.defaults.headers.post['Content-type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
